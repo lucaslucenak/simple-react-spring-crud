8adc2c8eeb33155edb42d0ce51de7fa0b3da9156
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-30 22:02:41 -0300

    Update server: add files

---
+++ server/src/test/java/com/lucaslucena/simplecrudapi/SimpleCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslucena.simplecrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
