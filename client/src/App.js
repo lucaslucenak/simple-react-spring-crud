737479c98ca26ab95e1a54dfe65b11f1f3cda458
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-01 02:32:27 -0300

    Update client: Add reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import PostEmployee from './components/postEmployee/PostEmployee';
 import GetAllEmployees from './components/getAllEmployees/GetAllEmployees';
 import Home from './components/home/Home';
+import {Button} from 'reactstrap'
 
 
 function App() {
   return (
     <Router>
       <nav>
+        
         <Link to="/"> Home </Link>
-        <Link to="/post-employee"> Post employee </Link>
+        <Button color="primary"> <Link to="/post-employee"> Post employee </Link> </Button>
         <Link to="get-all-employees"> Get all employees </Link>
       </nav>
       <Routes>
